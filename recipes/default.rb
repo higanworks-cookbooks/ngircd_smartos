960c97dff38785de8ad1b0a4c46ff61a14969e0d
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-08 14:48:07 +0900

    use lwrp to install

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,28 +5,7 @@
 # Copyright 2013, HiganWorks LLC
 #
 
-directory node['ngircd']['working_dir'] do
-  action :create
-end
-
-bash 'make and install ngircd' do
-  action :nothing
-  flags '-ex'
-  cwd node['ngircd']['working_dir']
-  code <<-EOH
-  tar xzf #{node['ngircd']['arch_file']}
-  cd #{::File.basename(node['ngircd']['arch_file'], '.tar.gz')}
-  ./configure #{node['ngircd']['configure_flags']}
-  make -j2
-  make install
-  EOH
-end
-
-remote_file ::File.join(node['ngircd']['working_dir'], node['ngircd']['arch_file'])   do
-  action :create_if_missing
-  source node['ngircd']['site_url'] + node['ngircd']['arch_file']
-  notifies :run, 'bash[make and install ngircd]', :immediately
-end
+include_recipe 'ngircd_smartos::binary'
 
 smf 'ngircd' do
   start_command '/opt/local/sbin/ngircd'
